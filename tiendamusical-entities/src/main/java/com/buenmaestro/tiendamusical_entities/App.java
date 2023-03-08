8316cafb8a832568f47d7c74cfdca6a30f8f448b
saulf01 <saulfloresccv01@gmail.com>
2023-03-07 20:24:36 -0600

    Version inicial del proyecto final tiendaMusical en java

---
+++ tiendamusical-entities/src/main/java/com/buenmaestro/tiendamusical_entities/App.java
@@ -0,0 +1,13 @@
+package com.buenmaestro.tiendamusical_entities;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ tiendamusical-services/src/main/java/com/buenmaestro/tiendamusical_services/App.java
@@ -0,0 +1,13 @@
+package com.buenmaestro.tiendamusical_services;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
